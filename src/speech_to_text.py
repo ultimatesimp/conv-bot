00d9ddb16357ec92946bbc468c07f299644d4172
ultimatesimp <adityabhargav203@gmail.com>
2024-01-21 21:14:23 +0530

    First Commit, basic code for recording speech and converting it to text is complete

---
+++ src/speech_to_text.py
@@ -0,0 +1,24 @@
+#import library
+import speech_recognition as sr
+#Initiаlize  reсоgnizer  сlаss  (fоr  reсоgnizing  the  sрeeсh)
+r = sr.Recognizer()
+# Reading Audio file as source
+#  listening  the  аudiо  file  аnd  stоre  in  аudiо_text  vаriаble
+with sr.AudioFile('wav_files\output.wav') as source:
+    audio_text = r.listen(source)
+# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+    try:
+        # using google speech recognition
+        text = r.recognize_google(audio_text)
+        print('Converting audio transcripts into text ...')
+        print(text)
+    except:
+         print('Sorry.. run again...')
+
+""" import speech_recognition
+
+class Speech_Recognizer:
+    def __init__(self): """
+
+
+
